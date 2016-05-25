eb1e29d661a6ef7dc05c227d68740dd2e4b51c9e
s.piskunov <gcyc10neg@gmail.com>
2016-05-25 11:49:08 +0300

    Add annotation to Photo.class

--- src/main/java/com/dudes/model/Photo.java
+++ src/main/java/com/dudes/model/Photo.java
@@ -1,10 +1,18 @@
 package com.dudes.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by vgrinyov on 23.05.16.
  */
+@Entity
 public class Photo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     public Photo() {
