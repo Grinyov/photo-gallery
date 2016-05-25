64d2cee2a7c9da1eb0cf4e3b9b546b97116febe1
s.piskunov <gcyc10neg@gmail.com>
2016-05-25 11:45:30 +0300

    Refactor class Photo

--- src/main/java/com/dudes/model/Photo.java
+++ src/main/java/com/dudes/model/Photo.java
@@ -1,7 +1,24 @@
 package com.dudes.model;
 
 /**
- * Created by s.piskunov on 23.05.2016.
+ * Created by vgrinyov on 23.05.16.
  */
 public class Photo {
+
+    private long id;
+
+    public Photo() {
+    }
+
+    public Photo(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
